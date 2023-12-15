68cb4ac57cc4d8f58dfa0b1121789255dce41aee
Or <60711866+oregev@users.noreply.github.com>
2023-12-15 12:13:24 +0200

    update router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,8 +4,8 @@ import { Quiz, Home, NotFound } from 'pages';
 
 export const AppRouter = (): JSX.Element => (
   <Routes>
+    <Route path="/" element={<Home />} />
     <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
       <Route path="quiz" element={<Quiz />} />
     </Route>
     <Route path="*" element={<NotFound />} />
