82c23642b805856fb952f34fdabeb100e664cd86
Or <60711866+oregev@users.noreply.github.com>
2023-12-15 13:15:31 +0200

    base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 import.meta.env.VITE_API_LOCALHOST;
 import.meta.env.VITE_API_PROD;
 
-const BASE_URL = import.meta.env.PROD
-  ? import.meta.env.VITE_API_PROD
-  : import.meta.env.VITE_API_LOCALHOST;
+// const BASE_URL = import.meta.env.PROD
+//   ? import.meta.env.VITE_API_PROD
+//   : import.meta.env.VITE_API_LOCALHOST;
 
 const baseConfig = {
   baseURL: 'https://trivia-api-nine.vercel.app/',
