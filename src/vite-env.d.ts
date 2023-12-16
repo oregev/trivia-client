d743db4bc6d90a7e43382dcdd0723221cf17a282
Or <60711866+oregev@users.noreply.github.com>
2023-12-16 16:54:56 +0200

    define envs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_LOCALHOST: string;
-  readonly VITE_API_PROD: string;
+  readonly VITE_APP_API: string;
 }
 
 interface ImportMeta {
